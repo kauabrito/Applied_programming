f7494d5f0958e56fbba37cc75a5db192e5601cff
Cauã Brito <137815542+kauabrito@users.noreply.github.com>
2024-12-09 22:11:30 -0300

    EMBARCA_TECH

---
+++ EMBARCATECH/capitulo03_code1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int valor = 30;
+void funcao_teste()
+{
+    printf("Teste");
+}
+
+int main()
+{
+    printf("EMBARCA_TECH %d\n", valor);
+    funcao_teste();
+}
